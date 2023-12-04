0324c3a1145dda6733c16297a8f5861fd9824448
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-12-04 08:37:30 +0000

    feat: part 1/day 1

--- day1/src/bin/part1.rs
+++ day1/src/bin/part1.rs
@@ -1,3 +1,43 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../../input/part1.txt");
+    let result = process(input);
+    println!("Result (part1): {}", result);
+}
+
+fn process(input: &str) -> String {
+    let digit = Regex::new(r"\d").unwrap();
+    let mut sum = 0;
+    for line in input.lines() {
+        let mut digits = digit.find_iter(line);
+        let first = digits.next().unwrap().as_str();
+        let last = match digits.last() {
+            Some(x) => x.as_str(),
+            None => first,
+        };
+
+        let number = format!("{first}{last}").parse::<i32>().unwrap();
+        sum += number;
+    }
+
+    sum.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        assert_eq!(
+            process(
+                "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
+            ),
+            "142"
+        );
+    }
 }
